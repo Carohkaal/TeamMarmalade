bd7b0cbb4cf7e55c7d880c3e8b317f4e9edbf1ed
gjkaal <gjhkaal@gmail.com>
2023-02-03 22:30:29 +0100

    Setup web api & client

---
+++ WebApi/Rooting.WebApi/Rooting.WebApi.Client/UseClient.cs
@@ -0,0 +1,20 @@
+﻿using Rooting.Api;
+
+namespace Rooting.WebApi.Client
+{
+    public class UseClient
+    {
+        private readonly HttpClient _httpClient = new();
+
+        public UseClient()
+        {
+        }
+
+        public async Task<WeatherForecast[]> CurrentForecastAsync()
+        {
+            var c = new RootingClient("https://rootingwebapi.azurewebsites.net/", _httpClient);
+            var fc = await c.GetWeatherForecastAsync();
+            return fc.ToArray();
+        }
+    }
+}
