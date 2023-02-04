5cb69298b53f90686947cc35020ab9ddb2654855
gjkaal <gjhkaal@gmail.com>
2023-02-04 14:09:51 +0100

    Add mono game engine

--- WebApi/Rooting.WebApi/Rooting.WebApi.Client/UseClient.cs
+++ WebApi/Rooting.WebApi/Rooting.WebApi.Client/UseClient.cs
@@ -1,19 +1,19 @@
-﻿using Rooting.WebApi.Client.Api;
-using Rooting.WebApi.Client.Model;
-
-namespace Rooting.WebApi.Client
+﻿namespace Rooting.WebApi.Client
 {
     public class UseClient
     {
-        private readonly PlayerApi _apiClient = new PlayerApi("https://rootingwebapi.azurewebsites.net/");
+        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly RootingWebApiClient _apiClient;
 
         public UseClient()
         {
+            _httpClient.BaseAddress = new Uri("https://rootingwebapi.azurewebsites.net");
+            _apiClient = new RootingWebApiClient(_httpClient);
         }
 
         public async Task<PlayerModel[]> CurrentPlayersAsync()
         {
-            var fc = await _apiClient.PlayerCurrentPlayersGetAsync();
+            var fc = await _apiClient.CurrentPlayersAsync();
             return fc.ToArray();
         }
     }
