522520ddf912afc80964dcb22a3275dec59b6360
gjkaal <gjhkaal@gmail.com>
2023-02-04 14:20:11 +0100

    Add webapi client in monogame

--- WebApi/Rooting.Desktop/Rooting.Desktop/Game1.cs
+++ WebApi/Rooting.Desktop/Rooting.Desktop/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Linq;
+using System.Net.Http;
 
 namespace Rooting.Desktop
 {
@@ -8,17 +11,30 @@ namespace Rooting.Desktop
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private RootingWebApiClient _webApiClient;
+        private HttpClient _httpClient = new HttpClient();
+        private Uri serverUri = new Uri("https://rootingwebapi.azurewebsites.net");
+        private readonly Lazy<CardModel[]> _cardDefinitions;
+        private CardModel[] _cards;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            _httpClient.BaseAddress = serverUri;
+            _webApiClient = new RootingWebApiClient(_httpClient);
+            _cardDefinitions = new Lazy<CardModel[]>(() =>
+            {
+                var cards = _webApiClient.CardDefinitionsAsync().GetAwaiter().GetResult();
+                return cards.ToArray();
+            });
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _cards = _cardDefinitions.Value;
 
             base.Initialize();
         }
