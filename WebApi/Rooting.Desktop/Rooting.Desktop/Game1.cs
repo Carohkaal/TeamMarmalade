9a5b35bc59453f18f42087029e0824019c96ec13
Danny <danny.k@live.nl>
2023-02-04 15:28:31 +0100

    Removed godot files, added some content and retrieve current players

--- WebApi/Rooting.Desktop/Rooting.Desktop/Game1.cs
+++ WebApi/Rooting.Desktop/Rooting.Desktop/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Net.Http;
 
@@ -15,7 +16,11 @@ namespace Rooting.Desktop
         private HttpClient _httpClient = new HttpClient();
         private Uri serverUri = new Uri("https://rootingwebapi.azurewebsites.net");
         private readonly Lazy<CardModel[]> _cardDefinitions;
+        private readonly Lazy<PlayerModel[]> _currentPlayers;
         private CardModel[] _cards;
+        private PlayerModel[] _players;
+
+        private Texture2D cardTexture;
 
         public Game1()
         {
@@ -29,12 +34,19 @@ namespace Rooting.Desktop
                 var cards = _webApiClient.CardDefinitionsAsync().GetAwaiter().GetResult();
                 return cards.ToArray();
             });
+
+            _currentPlayers = new Lazy<PlayerModel[]>(() =>
+            {
+                var players = _webApiClient.CurrentPlayersAsync().GetAwaiter().GetResult();
+                return players.ToArray();
+            });
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
             _cards = _cardDefinitions.Value;
+            _players = _currentPlayers.Value;
 
             base.Initialize();
         }
@@ -42,6 +54,8 @@ namespace Rooting.Desktop
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            cardTexture = Content.Load<Texture2D>("Card1");
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -59,6 +73,10 @@ namespace Rooting.Desktop
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(cardTexture, new Vector2(0, 0), Color.White);
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
