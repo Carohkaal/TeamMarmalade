d245e68281fead5a1b2b0924c05f6437bca6be50
gjkaal <gjhkaal@gmail.com>
2023-02-06 16:35:14 +0100

    Create WPF application

---
+++ WebApi/Turnip/Turnip/App.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Turnip
+{
+    public interface IMainWindow
+    {
+        void Show();
+    }
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly ServiceProvider container;
+        private readonly ILogger<Application> logger;
+
+        public App()
+        {
+            logger = NullLogger<Application>.Instance;
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var options = new ServiceProviderOptions { ValidateOnBuild = true, ValidateScopes = true };
+            container = serviceCollection.BuildServiceProvider(options);
+        }
+
+        private void ConfigureServices(IServiceCollection container)
+        {
+            container.AddLogging(
+                o =>
+                {
+                    o.AddConsole();
+                }
+            );
+            container.AddTransient<IMainWindow, MainWindow>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = container.GetService<IMainWindow>();
+            if (mainWindow == null)
+            {
+                logger.LogCritical("No main window");
+                Current.Shutdown();
+            }
+            else
+            {
+                Application.Current.MainWindow = (Window)mainWindow;
+                Application.Current.MainWindow.Show();
+            }
+        }
+    }
+}
---
+++ WebApi/Turnip/Turnip/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Printing.Interop;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Turnip
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IMainWindow
+    {
+        private readonly ILogger<MainWindow> logger;
+
+        public MainWindow(ILogger<MainWindow> logger)
+        {
+            this.logger = logger;
+            this.WindowState = WindowState.Maximized;
+            this.WindowStyle = WindowStyle.None;
+            InitializeComponent();
+
+            btnQuit.Click += OnQuit;
+            btnResign.Click += OnResign;
+        }
+
+        private void CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            if (button != null)
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
+
+        private void OnResign(object sender, RoutedEventArgs e)
+        {
+            logger.LogInformation("Resigning game.");
+
+            var imageUri = ResourceAccessor.Get("content/graphics/cards/cover.300x400.png");
+            var bitmap = new BitmapImage(imageUri);
+            card4.Source = bitmap;
+        }
+
+        private void OnQuit(object sender, RoutedEventArgs e)
+        {
+            logger.LogInformation("Stopping application.");
+            Application.Current.Shutdown();
+        }
+    }
+}
